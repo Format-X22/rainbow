d793b6cf029dde26848359bda782f05215396161
Oleg <oleg.pav.m@gmail.com>
2018-02-12 11:51:33 +0300

    Loader.

---
+++ loader.rb
@@ -0,0 +1,42 @@
+require 'json'
+require 'http'
+require 'time'
+require 'active_support/core_ext/numeric/time'
+require 'active_support/core_ext/integer/time'
+
+#https://www.bitmex.com/api/udf/history?symbol=XBTUSD&resolution=5&from=1517963045&to=1518395105
+
+url = 'https://www.bitmex.com/api/udf/history?symbol=XBTUSD'
+mul = 10000 * 60
+
+resolution = 5
+from_start = 365.days.ago.to_i
+to_end = Time.now.to_i
+from = from_start
+to = from_start + mul * resolution
+
+result_hash = {}
+result = []
+
+loop do
+	if to > to_end
+		break
+	end
+
+	data = JSON.parse HTTP.get("#{url}&resolution=#{resolution}&from=#{from}&to=#{to}").to_s
+
+	puts data['t'].length
+
+	data['t'].each.with_index do |date, index|
+		result_hash[date] = [date, data['o'][index], data['c'][index], data['h'][index], data['l'][index]]
+	end
+
+	from = from + (mul * resolution) - 1000
+	to = from + (mul * resolution) - 1000
+end
+
+result_hash.each do |key, data|
+	result << data
+end
+
+File.write 'data.txt', Marshal.dump(result)
